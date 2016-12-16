c38080ea4d8e8f7d8a9ebc8f15bc9faff049b114
renwei <renwei@mergesecrity.com>
2016-12-16 19:59:59 +0800

    +resnet , #72

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -14,7 +14,8 @@ export PYTHONUNBUFFERED="True"
 
 DEV=GPU
 DEV_ID=0
-NET=VGG16
+# NET = VGG16 or resnet
+NET=resnet
 DATASET=pascal_voc
 
 array=( $@ )
@@ -48,26 +49,43 @@ LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
+
+if [ ${NET} = "VGG16" ]; then
+  NETWORK=VGGnet_train
 NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/"
 #NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/VGGnet_fast_rcnn_iter_30000.ckpt"
 #NET_INIT=~/data/imagenet_models/VGG_imagenet.npy
 #NET_INIT="/home/merge/data/imagenet_models/VGG_imagenet.npy"
+
+elif [ ${NET} = "resnet" ]; then
+  NETWORK=resnet_train
+
+# download from https://github.com/miraclebiu/TFFRCN_resnet50
+NET_INIT=/home/merge/data/imagenet_models/Resnet__iter_200000.ckpt
+
+fi
 
 time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
   --imdb ${TRAIN_IMDB} \
   --weights ${NET_INIT} \
   --iters ${ITERS} \
   --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-  --network VGGnet_train \
+  --network ${NETWORK} \
   ${EXTRA_ARGS}
 
 set +x
 NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
 set -x
 
+if [ ${NET} = "VGG16" ]; then
+  NETWORK=VGGnet_test
+elif [ ${NET} = "resnet" ]; then
+  NETWORK=resnet_test
+fi
+
 time python ./tools/test_net.py --device ${DEV} --device_id ${DEV_ID} \
   --weights ${NET_FINAL} \
   --imdb ${TEST_IMDB} \
   --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-  --network VGGnet_test \
+  --network ${NETWORK} \
   ${EXTRA_ARGS}
--- lib/networks/factory.py
+++ lib/networks/factory.py
@@ -9,8 +9,6 @@
 
 __sets = {}
 
-import networks.VGGnet_train
-import networks.VGGnet_test
 import pdb
 import tensorflow as tf
 
@@ -24,13 +22,26 @@ def get_network(name):
     #if not __sets.has_key(name):
     #    raise KeyError('Unknown dataset: {}'.format(name))
     #return __sets[name]
-    if name.split('_')[1] == 'test':
-       return networks.VGGnet_test()
-    elif name.split('_')[1] == 'train':
-       return networks.VGGnet_train()
+    if name.split('_')[0] == 'VGGnet':
+        if name.split('_')[1] == 'test':
+            import networks.VGGnet_test
+            return networks.VGGnet_test()
+        elif name.split('_')[1] == 'train':
+            import networks.VGGnet_train
+            return networks.VGGnet_train()
+        else:
+           raise KeyError('Unknown dataset: {}'.format(name))
+    elif name.split('_')[0] == 'resnet':
+        if name.split('_')[1] == 'test':
+            import networks.resnet_test
+            return networks.resnet_test()
+        elif name.split('_')[1] == 'train':
+            import networks.resnet_train
+            return networks.resnet_train.resnet_train()
+        else:
+            raise KeyError('Unknown dataset: {}'.format(name))
     else:
-       raise KeyError('Unknown dataset: {}'.format(name))
-    
+        raise KeyError('Unknown dataset: {}'.format(name))
 
 def list_networks():
     """List all registered imdbs."""
