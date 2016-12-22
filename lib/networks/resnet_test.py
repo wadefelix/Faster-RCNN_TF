ba255c4dc6c6c483779c69e1daebfc2472be8f98
renwei <renwei@mergesecrity.com>
2016-12-22 16:13:23 +0800

    fixed #72

--- lib/networks/resnet_test.py
+++ lib/networks/resnet_test.py
@@ -33,11 +33,11 @@ class resnet_test(resnet_base):
         (self.residual_block('pool1', 'res2a', 64, 256, projection=True, trainable=False)
          .residual_block('res2a', 'res2b', 64, 256, projection=False, trainable=False)
          .residual_block('res2b', 'res2c', 64, 256, projection=False, trainable=False)
-         .residual_block('res2c', 'res3a', 128, 512, projection=True, trainable=False)
+         .residual_block('res2c', 'res3a', 128, 512, projection=True, trainable=False, padding='VALID')
          .residual_block('res3a', 'res3b', 128, 512, projection=False, trainable=False)
          .residual_block('res3b', 'res3c', 128, 512, projection=False, trainable=False)
          .residual_block('res3c', 'res3d', 128, 512, projection=False, trainable=False)
-         .residual_block('res3d', 'res4a', 256, 1024, projection=True, trainable=False)
+         .residual_block('res3d', 'res4a', 256, 1024, projection=True, trainable=False, padding='VALID')
          .residual_block('res4a', 'res4b', 256, 1024, projection=False, trainable=False)
          .residual_block('res4b', 'res4c', 256, 1024, projection=False, trainable=False)
          .residual_block('res4c', 'res4d', 256, 1024, projection=False, trainable=False)
@@ -46,7 +46,7 @@ class resnet_test(resnet_base):
          )
 
         #========= RPN ============
-        (self.feed('res4f')
+        (self.feed('res4f_relu')
              .conv(3,3,512,1,1,name='rpn_conv/3x3',relu=True, bn=False)
              .conv(1,1,len(anchor_scales)*3*2 ,1 , 1, padding='VALID', relu = False, name='rpn_cls_score', bn=False))
 
@@ -67,13 +67,13 @@ class resnet_test(resnet_base):
              .proposal_layer(_feat_stride, anchor_scales, 'TRAIN',name = 'rois'))
 
         #========= RCNN ============
-        (self.feed('res4f', 'rois')
-             .roi_pool(7, 7, 1.0/16, name='roi_pool')
-         .residual_block('roi_pool', 'res5a', 512, 2048, projection=True, trainable=True)
+        (self.feed('res4f_relu', 'rois')
+             .roi_pool(7, 7, 1.0/16, name='res5a_branch2a_roipooling')
+         .residual_block('res5a_branch2a_roipooling', 'res5a', 512, 2048, projection=True, trainable=True, padding='VALID')
          .residual_block('res5a', 'res5b', 512, 2048, projection=False, trainable=True)
          .residual_block('res5b', 'res5c', 512, 2048, projection=False, trainable=True)
          .fc(n_classes, relu=False, name='cls_score')
          .softmax(name='cls_prob')
          )
-        (self.feed('pool5')
+        (self.feed('res5c_relu')
              .fc(n_classes*4, relu=False, name='bbox_pred'))
