9eb607937ebe7a27bb1aeaaca6641760c4645fda
renwei <renwei@mergesecrity.com>
2016-12-23 14:01:59 +0800

    fixed #74, tensorboard support

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -29,7 +29,6 @@ case $DATASET in
   pascal_voc)
     TRAIN_IMDB="voc_6829_train"
     TEST_IMDB="voc_2007_test"
-    PT_DIR="pascal_voc"
     ITERS=200
     ;;
   coco)
@@ -38,7 +37,6 @@ case $DATASET in
     # time to the LR drop (set in the solver to 350,000 iterations).
     TRAIN_IMDB="coco_2014_train"
     TEST_IMDB="coco_2014_minival"
-    PT_DIR="coco"
     ITERS=490000
     ;;
   *)
@@ -53,22 +51,22 @@ echo Logging output to "$LOG"
 
 
 if [ ${NET} = "VGG16" ]; then
-  NETWORK=VGGnet_train
-NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/"
-#NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/VGGnet_fast_rcnn_iter_30000.ckpt"
-#NET_INIT=~/data/imagenet_models/VGG_imagenet.npy
-#NET_INIT="/home/merge/data/imagenet_models/VGG_imagenet.npy"
+    NETWORK=VGGnet_train
+    NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/"
+    #NET_INIT="/home/merge/Faster-RCNN_TF/output/faster_rcnn_end2end/voc_6829_train/VGGnet_fast_rcnn_iter_30000.ckpt"
+    #NET_INIT=~/data/imagenet_models/VGG_imagenet.npy
+    #NET_INIT="/home/merge/data/imagenet_models/VGG_imagenet.npy"
 
 elif [ ${NET} = "resnet" ]; then
-  NETWORK=Resnet50_train
-  #NETWORK=resnet_train
+    #NETWORK=Resnet50_train
+    NETWORK=resnet_train
+    # download from https://github.com/miraclebiu/TFFRCN_resnet50
+    NET_INIT=/home/merge/data/imagenet_models/Resnet50.npy
+fi
 
 TENSORBOARDLOGDIR=sameasoutput
-#TENSORBOARDLOGDIR=~/resnet_logs/
-# download from https://github.com/miraclebiu/TFFRCN_resnet50
-NET_INIT=/home/merge/data/imagenet_models/Resnet50.npy
-
-fi
+# python ~/.local/lib/python2.7/site-packages/tensorflow/tensorboard/tensorboard.py --logdir=/path/to/logdir
+# default port 6006, `--port=xxxx` to specify port
 
 time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
   --imdb ${TRAIN_IMDB} \
@@ -86,7 +84,7 @@ set -x
 if [ ${NET} = "VGG16" ]; then
   NETWORK=VGGnet_test
 elif [ ${NET} = "resnet" ]; then
-  NETWORK=Resnet50_test
+  NETWORK=resnet_test
 fi
 
 python ./tools/gapttest.py --net ${NETWORK} \
